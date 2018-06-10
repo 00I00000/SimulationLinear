55e3517365c4baf6cf7a9a561442b2e5163a9a56
Frank Ling <f.ling336@gmail.com>
2018-06-10 13:53:54 +0200

    everything fixed + object oriented

--- src/app/Collision.java
+++ src/app/Collision.java
@@ -2,7 +2,6 @@ package app;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
@@ -18,32 +17,10 @@ public class Collision extends JFrame {
 		backBuffer = new BufferedImage(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT, BufferedImage.TYPE_INT_RGB);
 	}
 	
-	public boolean intersected = false;
-	
-	public int yC = Constants.WINDOW_HEIGHT / 2;
-	public int xC1 = 0;
-	public int xC2 = Constants.WINDOW_WIDTH / 2;
-	public int yVelocity = 0;
-	public double xVel1 = 50.0;
-	public double xVel2 = 0.0;
-	public double trueXCoord1 = xC1;
-	public double trueXCoord2 = xC2;
-	
-	public double xCenterOfMass = (xC1 + xC2) / 2;
+	public static Object obj1 = new Object(0, Constants.WINDOW_HEIGHT / 2, 20, 20, 50, 20);
+	public static Object obj2 = new Object(Constants.WINDOW_WIDTH / 2, Constants.WINDOW_HEIGHT / 2, 20, 20, 0, 20);
 	
 	double timeLastFrame = 0;
-	
-	public double centerOfMass(double mass1, double mass2, double velocity1, double velocity2) {
-		return (mass1 * velocity1 + mass2 * velocity2) / (mass1 + mass2);
-	}
-	
-	public double velAfter1(double mass1, double mass2, double vel1, double vel2, double epsilon) {
-		return centerOfMass(mass1, mass2, vel1, vel2) - epsilon * mass2 / (mass1 + mass2)*(vel2 - vel1);
-	}
-	
-	public double velAfter2(double mass1, double mass2, double vel1, double vel2, double epsilon) {
-		return centerOfMass(mass1, mass2, vel1, vel2) + epsilon * mass2 / (mass1 + mass2)*(vel2 - vel1);
-	}
 	
 	//x and y in fill() are starting points right and top
 	
@@ -51,42 +28,28 @@ public class Collision extends JFrame {
 		Graphics g = getGraphics();
 		Graphics bbg = backBuffer.getGraphics();
 		
-		Rectangle hitbox1 = new Rectangle(xC1, yC, 20, 20);
-		Rectangle hitbox2 = new Rectangle(xC2, yC, 20, 20);
-		
 		double deltaTime = (absT - timeLastFrame);
 		timeLastFrame = absT;
+		
 		bbg.setColor(Color.WHITE);
 		bbg.fillRect(0, 0, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
 		bbg.setColor(Color.RED);
-		double velocityAfter1 = velAfter1(20.0, 20.0, xVel1, xVel2, 0);
-		// x = x(0) + v * t;
-		double velocityDelta1 = velocityAfter1 * deltaTime;
-		double velocityAfter2 = velAfter2(20.0, 20.0, xVel1, xVel2, 0);
-		double velocityDelta2 = velocityAfter2 * deltaTime;
-		if (hitbox1.intersects(hitbox2)) {
-			intersected = true;
+		
+		if (obj1.getHitbox().intersects(obj2.getHitbox())) {
+			Object.intersected = true;
 		}
-		if (intersected) {
-			trueXCoord1 += velocityDelta1;
-			trueXCoord2 += velocityDelta2;
-			xC1 = (int) trueXCoord1;
-			hitbox1.x = xC1;
-			xC2 = (int) trueXCoord2;
-			hitbox2.x = xC2;
+		if (Object.intersected) {
+			obj1.velDelta1(obj2, Object.epsilon, deltaTime);
+			obj2.velDelta2(obj1, Object.epsilon, deltaTime);
 		} else {
-			trueXCoord1 += xVel1 * deltaTime;
-			xC1 = (int) trueXCoord1;
-			hitbox1.x = xC1;
-			trueXCoord2 += xVel2 * deltaTime;
-			xC2 = (int) trueXCoord2;
-			hitbox2.x = xC2;
+			obj1.vel(deltaTime);
+			obj2.vel(deltaTime);
 		}
-		xCenterOfMass += (centerOfMass(20.0, 20.0, xVel1, xVel2) * deltaTime);
-		System.out.println(xCenterOfMass);
-		bbg.fillOval(((int) (xCenterOfMass)) - 5, Constants.WINDOW_HEIGHT / 2 + 40, 10, 10);
-		bbg.fillOval(xC1, yC, 20, 20);
-		bbg.fillOval(xC2, yC, 20, 20);
+		Object.moveCOM(deltaTime);
+		int cast = (int) Object.xCenterOfMass;
+		bbg.fillOval(cast - 5, Constants.WINDOW_HEIGHT / 2 + 40, 10, 10);
+		bbg.fillOval(obj1.getX(), obj1.getY(), obj1.getWidth(), obj1.getHeight());
+		bbg.fillOval(obj2.getX(), obj2.getY(), obj2.getWidth(), obj2.getHeight());
 		
 		g.drawImage(backBuffer, 0, 0, null);
 		
@@ -95,9 +58,12 @@ public class Collision extends JFrame {
 	
 	public static void main(String[] args) {
 		Collision test = new Collision();
-		
 		test.setVisible(true);
 		double absT = 0;
+		obj1.centerOfMass(obj2);
+		Object.epsilon = 1;
+		Object.xCenterOfMass = (obj1.getX() + obj2.getX() + obj1.getWidth()) / 2;
+		System.out.println(obj1.velAfter1(obj2, Object.epsilon));
 		while (true) {
 			test.draw(absT);
 			try {
---
+++ src/app/Object.java
@@ -0,0 +1,111 @@
+package app;
+
+import java.awt.Rectangle;
+
+public class Object {
+	
+	public static double centerOfMass;
+	public static double xCenterOfMass;
+	public static boolean intersected = false;
+	public static double epsilon = 0;
+	
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	
+	private double exactX;
+	
+	private double vel;
+	private double mass;
+	
+	private Rectangle hitbox;
+	
+	public Object(int x, int y, int width, int height, double vel, double mass) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.vel = vel;
+		this.mass = mass;
+		exactX = x;
+		hitbox = new Rectangle(x, y, width, height);
+	}
+	
+	public void centerOfMass(Object obj) {
+		centerOfMass = (mass * vel + obj.mass * obj.vel) / (mass + obj.mass);
+	}
+	
+	public static void moveCOM(double deltaTime) {
+		xCenterOfMass += centerOfMass * deltaTime;
+	}
+	
+	public void vel(double deltaTime) {
+		exactX += vel * deltaTime;
+		x = (int) exactX;
+		hitbox.x = x;
+	}
+	
+	/*public double velAfter1(Object obj, double epsilon) {
+		return centerOfMass(obj) - epsilon * obj.vel / (mass + obj.mass) * (obj.vel - vel);
+	}
+	
+	public double velAfter2(Object obj, double epsilon) {
+		return centerOfMass(obj) + epsilon * obj.vel / (mass + obj.mass) * (obj.vel - vel);
+	}*/
+	
+	public double velAfter1(Object obj, double epsilon) {
+		return centerOfMass + epsilon * obj.mass / (mass + obj.mass) * (obj.vel - vel);
+	}
+	
+	public double velAfter2(Object obj, double epsilon) {
+		return centerOfMass - epsilon * mass / (obj.mass + mass) * (vel - obj.vel);
+	}
+	
+	public void velDelta1(Object obj, double epsilon, double deltaTime) {
+		exactX += velAfter1(obj, epsilon) * deltaTime;;
+		x = (int) exactX;
+		hitbox.x = x;
+	}
+	
+	public void velDelta2(Object obj, double epsilon, double deltaTime) {
+		exactX += velAfter2(obj, epsilon) * deltaTime;;
+		x = (int) exactX;
+		hitbox.x = x;
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public double getExactX() {
+		return exactX;
+	}
+
+	public double getVel() {
+		return vel;
+	}
+
+	public double getMass() {
+		return mass;
+	}
+	
+	public Rectangle getHitbox() {
+		return hitbox;
+	}
+	
+	
+
+}
